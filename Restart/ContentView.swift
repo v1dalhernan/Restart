5fddd502afb4c39c64e646a3d2fa3ba6ac7be4ae
Jhonathan Vidal <jhonathanvidalvv@gmail.com>
2025-05-12 15:19:29 -0500

    Initial Commit

---
+++ Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Jhonathan Vidal on 05/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jhonathan Vidal on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
